60367d9121912f29f49f5aec0112c9f304c723fc
agata <agata@af17e6e2-40f0-4ed2-8940-35b5f8ea79a1>
2007-07-17 09:33:40 +0000

    不必要な定数を削除
    git-svn-id: https://www.seasar.org/svn/sandbox/s2xmlrpc/trunk@12 af17e6e2-40f0-4ed2-8940-35b5f8ea79a1

--- s2xmlrpc/src/main/java/org/seasar/remoting/xmlrpc/S2XmlRpcConstants.java
+++ s2xmlrpc/src/main/java/org/seasar/remoting/xmlrpc/S2XmlRpcConstants.java
@@ -23,64 +23,9 @@ package org.seasar.remoting.xmlrpc;
 public interface S2XmlRpcConstants {
 
     /**
-     * オペレーション名の名前空間URI
-     */
-    String OPERATION_NAMESPACE_URI    = "http://soapinterop.org/";
-
-    /**
-     * diconファイルで、使用するAxisエンジンを指示するために <code>&lt;meta&gt;</code> 要素に指定する
-     * <code>name</code> 属性値のローカル名(接頭辞 <code>"axis-"</code> の後ろ)です。
-     */
-    String META_ENGINE                = "engine";
-
-    /**
-     * diconファイルで、コンポーネントがAxisサービスであることを示すために <code>&lt;meta&gt;</code>
-     * 要素に指定する <code>name</code> 属性値のローカル名(接頭辞 <code>"xmlrpc-"</code> の後ろ)です。
+     * diconファイルで、コンポーネントがXML-RPCで公開されるコンポーネントであることを示すために <code>&lt;meta&gt;</code>
+     * 要素に指定する <code>name</code> 属性値です。
      */
     String META_SERVICE               = "service";
 
-    /**
-     * diconファイルで、コンポーネントがAxisハンドラであることを示すために <code>&lt;meta&gt;</code>
-     * 要素に指定する <code>name</code> 属性値のローカル名(接頭辞 <code>"axis-"</code> の後ろ)です。
-     */
-    String META_HANDLER               = "handler";
-
-    /**
-     * S2Axisがインスタンス管理を行うRPCプロバイダのローカル名です。
-     */
-    String PROVIDER_S2RPC             = "S2RPC";
-
-    /**
-     * S2Axisがインスタンス管理を行うMSGプロバイダのローカル名です。
-     */
-    String PROVIDER_S2MSG             = "S2MSG";
-
-    /**
-     * デフォルトのAxisエンジンを使用することを示します。
-     */
-    String ENGINE_DEFAULT             = "default";
-
-    /**
-     * デフォルトのAxisクライアントエンジンを使用することを示します。
-     */
-    String ENGINE_DEFAULT_CLIENT      = "default-client";
-
-    /**
-     * デフォルトのAxisサーバエンジンを使用することを示します。
-     */
-    String ENGINE_DEFAULT_SERVER      = "default-server";
-
-    /**
-     * サーブレットコンテキストからAxisエンジンを取得することを示します。
-     */
-    String ENGINE_FROM_SERVLET        = "servlet:";
-
-    /** S2コンテナからAxisエンジンを取得することを示します。 */
-    String ENGINE_FROM_S2CONTAINER    = "s2:";
-
-    /** AxisServletのサーブレット名 */
-    String AXIS_SERVLET               = "AxisServlet";
-
-    /** サーブレットコンテキストからConfigurationContextを取得するためのキー */
-    String ATTR_CONFIGURATION_CONTEXT = "CONFIGURATION_CONTEXT";
 }
